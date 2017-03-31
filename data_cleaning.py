b51831070932a78368aa58b49ab20e53cb2414d1
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-03-31 02:13:42 -0400

    Minor changes

--- data_cleaning.py
+++ data_cleaning.py
@@ -18,7 +18,7 @@ start_time = time.time()
 
 def print_time(label):
     print "-----------------------------"
-    print label + ": " + str(time.time() - start_time) + "seconds"
+    print label + ": " + str(time.time() - start_time) + " seconds"
     print "-----------------------------"
 
 # In[13]:
